6aa70d17c63001b4ade1242c3668dcad4a5f8032
salt338-03 <salt338@naver.com>
2025-01-06 12:26:55 +0900

    초기 커밋.

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.worker = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.txtExtension = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Chocolate;
+            this.progressBar1.Location = new System.Drawing.Point(12, 25);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(451, 23);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(12, 90);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(370, 21);
+            this.txtDirectory.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(388, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.ItemHeight = 12;
+            this.lstResults.Location = new System.Drawing.Point(12, 155);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(727, 88);
+            this.lstResults.TabIndex = 4;
+            // 
+            // txtExtension
+            // 
+            this.txtExtension.Location = new System.Drawing.Point(12, 119);
+            this.txtExtension.Name = "txtExtension";
+            this.txtExtension.Size = new System.Drawing.Size(370, 21);
+            this.txtExtension.TabIndex = 5;
+            this.txtExtension.Text = "기본 설정은 전부";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 266);
+            this.Controls.Add(this.txtExtension);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker worker;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.TextBox txtExtension;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.worker = new BackgroundWorker();
+            // BackgroundWorker의 ReportProgress() 메서드 활용 여부, 보통 true
+            this.worker.WorkerReportsProgress = true;
+            // CancelAsync()로 BackgroundWorker를 멈출 수 있게 할지, 보통 true
+            this.worker.WorkerSupportsCancellation = true;
+
+            // BackgroundWorker가 UI스레드와 별개로 수행할 메서드
+            this.worker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+            // ReportProgress() 메서드가 수행될 때 실행될 메서드
+            this.worker.ProgressChanged += new ProgressChangedEventHandler(Worker_ProgressChanged);
+            // ReportProgress()가 100으로 호출되면 마지막에 한 번 실행되는 메서드
+            this.worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Worker_Complete);
+            txtExtension.Text = "*";
+        }
+
+        void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string directoryPath = txtDirectory.Text;
+            string fileExtension = txtExtension.Text;
+
+            if (string.IsNullOrWhiteSpace(directoryPath) || !Directory.Exists(directoryPath))
+            {
+                MessageBox.Show("유효한 디렉토리 경로를 입력하세요.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(fileExtension))
+            {
+                MessageBox.Show("확장자를 입력하세요.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                var files = Directory.GetFiles(directoryPath, $"*{fileExtension}", SearchOption.AllDirectories);
+                lstResults.Items.Clear();
+                foreach (var file in files)
+                {
+                    lstResults.Items.Add(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"오류 발생: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            // 백그라운드 작업 실행 로직
+        }
+
+        void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        void Worker_Complete(object sender, EventArgs e)
+        {
+              }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!this.worker.IsBusy)
+            {
+                this.worker.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+        }
+    }
+}
