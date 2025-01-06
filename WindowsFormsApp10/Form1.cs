f214d122b082ddf38fa0e2c0a0ebbb80a7836f81
salt338-03 <salt338@naver.com>
2025-01-06 12:40:12 +0900

    수정

--- WindowsFormsApp10/Form1.cs
+++ WindowsFormsApp10/Form1.cs
@@ -51,7 +51,13 @@ namespace WindowsFormsApp10
 
             try
             {
-                var files = Directory.GetFiles(directoryPath, $"{fileExtension}", SearchOption.AllDirectories);
+                var files = Directory.GetFiles(directoryPath, $"*{fileExtension}", SearchOption.AllDirectories);
+                //* 와일드카드 GetFiles(string path, string searchPattern...) 
+                //path (필수)  검색할 디렉토리의 경로를 지정합니다.
+                //searchPattern (선택) 파일 이름이나 확장자를 기반으로 필터링하는 문자열입니다. 기본값은 "*"으로, 모든 파일을 의미합니다.
+                //SearchOption.TopDirectoryOnly: 지정된 디렉토리 내 파일만 검색.
+                //SearchOption.AllDirectories: 하위 디렉토리까지 포함하여 검색.
+
                 lstResults.Items.Clear();
                 foreach (var file in files)
                 {
